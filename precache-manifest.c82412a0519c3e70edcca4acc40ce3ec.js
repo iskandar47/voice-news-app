15f7d6836d8667d6079401d128a62f65f63a28c8
iskandar47 <salimdriai15@gmail.com>
2020-10-08 09:25:16 +0100

    Updates

---
+++ precache-manifest.c82412a0519c3e70edcca4acc40ce3ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc3233b2eb6c4220000f2e51fe9e6d0b",
+    "url": "/voice-news-app/index.html"
+  },
+  {
+    "revision": "f3a66d449af13e1d578e",
+    "url": "/voice-news-app/static/css/main.823cb4b7.chunk.css"
+  },
+  {
+    "revision": "bba457208c8b8e222083",
+    "url": "/voice-news-app/static/js/2.48506a43.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/voice-news-app/static/js/2.48506a43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3a66d449af13e1d578e",
+    "url": "/voice-news-app/static/js/main.9594b8a6.chunk.js"
+  },
+  {
+    "revision": "ff9fe928d1ff2ea4ed41",
+    "url": "/voice-news-app/static/js/runtime-main.2bd65f11.js"
+  }
+]);
