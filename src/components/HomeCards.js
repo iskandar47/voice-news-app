c4d2c0d2bbdd2b17a92d9fc89bc09645b27cda29
iskandar47 <salimdriai15@gmail.com>
2020-10-08 16:00:23 +0100

    command changes

--- src/components/HomeCards.js
+++ src/components/HomeCards.js
@@ -6,7 +6,7 @@ import NewsCard from "./NewsCard.js"
 const infoCards = [
     { title: 'Latest News All', info: 'news from all over the world inculdes all categories from global sources', text: 'Give me the latest news or show me the global news ' },
     { title: 'News by Categories', info: 'Business, Entertainment, General, Health, Science, Sports, Technology', text: 'Give me the latest Technology news or what\'s the latest news about sports' },
-    { title: 'News by Terms', info: 'Bitcoin, PlayStation 5, Smartphones, Donald Trump...', text: 'What\'s up with PlayStation 5 or give me the news about Smartphones' },
+    { title: 'News by Terms', info: 'Bitcoin, PlayStation 5, Smartphones, Donald Trump...', text: 'What\'s up with apple or give me the news about Smartphones' },
     { title: 'News by Sources', info: 'CNN, Wired, BBC News, Time, IGN, Buzzfeed, ABC News...', text: 'Give me the news from CNN of show me the latest news from ABC News' },
   ];
 
